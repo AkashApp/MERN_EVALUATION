c0f5a128e1130572ba685ef3884ffe42be716301
Akash Borate <27akashborate@gmail.com>
2022-04-17 17:06:17 +0530

    commit Home page 1

--- Frontend/classtime/src/components/Home/Home.jsx
+++ Frontend/classtime/src/components/Home/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import axios from "axios";
 
 export const Home = () => {
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    axios.get("http://localhost:7492/class").then((response) => {
+      //   console.log(response.data);
+      setItems(response.data);
+    });
+  }, []);
+  console.log(items);
   return <div>Home</div>;
 };
